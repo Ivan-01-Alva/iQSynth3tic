1aa550417fa359140c593a99d83259a2e5d4dc7e
iQ Synth3tic <ivansan.alva@gmail.com>
2024-10-09 23:01:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Actualizar el año en el pie de página
+document.getElementById('year').textContent = new Date().getFullYear();
+
+// Menú Responsive
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-active');
+    hamburger.classList.toggle('toggle');
+});
