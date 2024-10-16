1f881f6be8346aacf474771311d725401a8f91e8
iQ Synth3tic <ivansan.alva@gmail.com>
2024-10-16 23:09:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,18 @@ document.getElementById('year').textContent = new Date().getFullYear();
 // Menú Responsive
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
+document.addEventListener('DOMContentLoaded', function () {
+    const serviceItems = document.querySelectorAll('.service-item');
+
+    serviceItems.forEach(function (item) {
+        item.addEventListener('click', function () {
+            item.classList.toggle('flipped');
+        });
+    });
+});
+
 
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('nav-active');
     hamburger.classList.toggle('toggle');
 });
-
-// Función para voltear las tarjetas de servicios
-function toggleFlip(element) {
-    element.classList.toggle('flipped');
-}
